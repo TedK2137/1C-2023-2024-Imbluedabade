175696c353790debfdf6f81f4e68fb0d71077f64
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-10-17 09:32:46 +0200

    koniec lekcji xD

--- Programowanie/InstructionIfConsoleApplication/InstructionIfConsoleApplication.cpp
+++ Programowanie/InstructionIfConsoleApplication/InstructionIfConsoleApplication.cpp
@@ -32,11 +32,46 @@ void task1()
 
 }
 
+void task2()
+{
+	int NumeroUsuario;
+	std::cout << "Podaj liczbe:\n";
+	std::cin >> NumeroUsuario;
+	if (NumeroUsuario < 10 ) // NumeroUsuario != 0
+		std::cout << "Liczba jest parzysta";
+	else
+		std::cout << "Liczba jest nieparzysta";
+
+
+
+}
+
+void task3()
+{
+	int NumeroUno, NumeroDos;
+	std::cout << "Podaj liczbe:\n";
+	std::cin >> NumeroUno;
+	std::cout << "Podaj liczbe:\n";
+	std::cin >> NumeroDos;
+
+	if (NumeroDos != 0)
+	{
+		int cociente = NumeroUno / NumeroDos;
+			std::cout << "Wynik dzielenia " << cociente << "\n";
+	}
+	else
+		std::cout << "Dzielenie przez zero!!1!1!!11\n";
+
+}
 
 int main()
 {
 	//task();
-	task1();
+	//task1();
+	//task2();
+	task3();
+
 }
+
 
 
