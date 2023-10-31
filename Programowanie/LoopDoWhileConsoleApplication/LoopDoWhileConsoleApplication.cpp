e019eaee5a3aa1a86522f76c54de23632790284f
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-10-31 09:09:36 +0100

    kl

---
+++ Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
@@ -0,0 +1,57 @@
+
+#include <iostream>
+
+//Napisz program, który ma pobraæ od u¿ytkownika liczbê dodatni¹. 
+//Zabezpiecz program przed pobieraniem liczb ujemnych.
+void task1()
+{
+	int numberFromUser;
+	std::cout << "Podaj liczbê doodatni¹:\n";
+	std::cin >> numberFromUser;
+	if (numberFromUser < 0)
+	{
+		std::cout << "Podaj liczbê doodatni¹:\n";
+		std::cin >> numberFromUser;
+		if (numberFromUser < 0)
+		{
+			std::cout << "Podaj liczbê doodatni¹:\n";
+			std::cin >> numberFromUser;
+			if (numberFromUser < 0)
+			{
+				std::cout << "Podaj liczbê doodatni¹:\n";
+				std::cin >> numberFromUser;
+				if (numberFromUser < 0)
+				{
+					std::cout << "Podaj liczbê doodatni¹:\n";
+					std::cin >> numberFromUser;
+					//wklejamy ca³ego If'a
+				}
+			}
+		}
+	}
+
+	std::cout << "Liczba dodatnia pobrana od uzytkownika " << numberFromUser << "\n";
+}
+
+void task2()
+{
+	int NumberFromUser;
+
+	do
+	{
+		std::cout << "Podaj liczbê doodatni¹:\n";
+		std::cin >> numberFromUser;
+	} while (numberFromUser < 0)
+
+
+
+
+
+}
+
+
+int main()
+{
+    
+}
+
