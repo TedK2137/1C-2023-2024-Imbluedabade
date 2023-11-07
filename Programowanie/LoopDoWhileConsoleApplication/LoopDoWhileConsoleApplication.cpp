96efd87f45c9aea615b658e58f3b79f85fe3f489
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-11-07 09:53:05 +0100

    koniec lekcji

--- Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
+++ Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
@@ -35,23 +35,47 @@ void task1()
 
 void task2()
 {
-	int NumberFromUser;
+	const int LOWER_RANGE = 1;
+	const int UPPER_RANGE = 100;
+	int NumeroUsuario;
+		srand(time(NULL));
+	int randomNumber = rand() % (UPPER_RANGE - LOWER_RANGE + 1) + LOWER_RANGE;
 
 	do
 	{
-		std::cout << "Podaj liczbê doodatni¹:\n";
-		std::cin >> numberFromUser;
-	} while (numberFromUser < 0)
+		std::cout << "Podaj liczbe\n";
+		std::cin >> NumeroUsuario;
+		if (NumeroUsuario > randomNumber)
+			std::cout << "mnij\n";
+		if (NumeroUsuario < randomNumber)
+			std::cout << "wincyj\n";
+	} while (NumeroUsuario != randomNumber);
 
-
+			std::cout << "Zgadles liczbe";
 
 
 
 }
 
+void task3()
+{
+	unsigned long long upperRange, currentNumber;
+	std::cout << "Podaj górny zakres wiêkszy b¹dŸ równy 1\n";
+	std::cin >> upperRange;
+
+	currentNumber = 1;
+
+	do
+	{
+		std::cout << currentNumber << ", ";
+		currentNumber = (currentNumber + 1);
+	} while (upperRange >= currentNumber);
+
+}
 
 int main()
 {
-    
+	setlocale(LC_CTYPE, "Polish");
+	//task2();
+	task3();
 }
-
