71f68b98f4ed1a9446aede839acf2be61ffb5d21
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-11-24 14:25:17 +0100

    wololo

---
+++ Programowanie/LoopWhileConsoleApplication/LoopWhileConsoleApplication.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+
+void task1()
+{
+    int nwd, numeroUno, numeroSecundo;
+
+    std::cout << "Podaj liczbê:\n";
+    std::cin >> numeroUno;
+    std::cout << "Podaj drug¹ liczbê:\n";
+    std::cin >> numeroSecundo;
+
+    nwd = (numeroUno < numeroSecundo) ? numeroUno : numeroSecundo;
+
+    while (numeroSecundo % nwd != 0 || numeroUno % nwd != 0)
+    {
+        nwd--;
+    }
+    std::cout << "Nwd wynosi: " << nwd;
+}
+
+
+
+
+
+
+
+
+int main()
+{
+    setlocale(LC_CTYPE, "Polish");
+    task1();
+}
