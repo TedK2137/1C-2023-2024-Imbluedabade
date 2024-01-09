e8305b024c90cbd227f3fb4afa0d7a02f1ee75c4
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-01-09 09:51:23 +0100

    kl

---
+++ Programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
@@ -0,0 +1,44 @@
+// ArrayConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+void task1()
+{
+    const unsigned short ARRAY_SIZE = 5;
+    int numbers[ARRAY_SIZE];
+
+    for (int i = 0; i < ARRAY_SIZE; i++)
+    {
+        std::cout << "Podaj dan¹\n";
+        std::cin >> numbers[i];
+    }
+
+    std::cout << "Podane dane w odwrotnej kolejnoœci:\n";
+    for (int i = ARRAY_SIZE - 1; i >= 0; i--)
+    {
+    std::cout << numbers[i] << ", ";
+    }
+    std::cout << "\n";
+}
+
+//œrednia arytmetyczna elementów w tablicy liczb ca³kowitych
+void task2()
+{
+    const unsigned short LOWER_RANGE = 5;
+    const unsigned short UPPER_RANGE = 7;
+
+    const unsigned short ARRAY_SIZE = 30;
+    int numbers[ARRAY_SIZE];
+
+    srand(time(0));
+
+    std::cout
+
+}
+
+int main()
+{
+    setlocale(LC_CTYPE, "polish");
+    task1();
+}
