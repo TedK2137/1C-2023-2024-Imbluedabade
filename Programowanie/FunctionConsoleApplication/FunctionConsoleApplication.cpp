42468dd8882675263abe5cba77f92b112806fd39
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-01-31 08:58:57 +0100

    ae

--- Programowanie/FunctionConsoleApplication/FunctionConsoleApplication.cpp
+++ Programowanie/FunctionConsoleApplication/FunctionConsoleApplication.cpp
@@ -19,14 +19,45 @@ void task3(std::string name, int age)
     else
         std::cout << "jest niepelnoletni/niepelnoletnia\n";
 }
+
+void task4(int number)
+{
+    std::cout << "zmienna number w funkcji task4: " << number << "\n";
+    number--;
+    std::cout << "zmienna number w funkcji task4: " << number << "\n";
+}
+
+void task5(int &number)
+{
+    std::cout << "zmienna number w funkcji task5: " << number << "\n";
+    number--;
+    std::cout << "zmienna number w funkcji task5: " << number << "\n";
+}
+
 int main()
 {
-    //task1();
-    //task1();
-    //task2("Witaj swiecie!!\n");
-    //task2("Ala ma kota\n");
-    //std::string text = "Uczyæ siê programowania!!!\n";
-    //task2(text);
+    /*
+    task1();
+    task1();
+    task2("Witaj swiecie!!\n");
+    task2("Ala ma kota\n");
+    std::string text = "Uczyæ siê programowania!!!\n";
+    task2(text);
     task3("Jan", 35);
     task3("Ala", 17);
+    */
+    int number = 5;
+    /*std::cout << "zmienna number w funkcji main: " << number << "\n";
+    task4(number);
+    std::cout << "zmienna number w funkcji main: " << number << "\n";
+
+    task4(9);*/
+    const int NUMBER = 9;
+    //task4(NUMBER);
+
+    std::cout << "zmienna number w funkcji main: " << number << "\n";
+    task5(number);
+    std::cout << "zmienna number w funkcji main: " << number << "\n"
+
 }
+
