5b0bf086bfdb18a5650887eaa8ad23285601ba14
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-03-06 09:46:23 +0100

    aeae

---
+++ Programowanie/PointersConsoleApplication/PointersConsoleApplication.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void task1()
+{
+    int firstNumber = 48;
+    int secondNumber = 697;
+    int tab[2] = { 15 , 87 };
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
