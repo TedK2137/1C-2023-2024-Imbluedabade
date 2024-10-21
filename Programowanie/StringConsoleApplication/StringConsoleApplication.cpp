85096185f2552c58d5bffd0df920d8f4da296c79
q <q>
2024-10-21 09:54:41 +0200

    aeae

---
+++ Programowanie/StringConsoleApplication/StringConsoleApplication.cpp
@@ -0,0 +1,12 @@
+// StringConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_TYPE, "polish");
+
+	std::string text;
+
+}
