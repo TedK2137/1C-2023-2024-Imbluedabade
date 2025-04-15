3c7df10a0f30f5674769baeb39483bc8a6759616
TedK2137 <stachurakuba0@gmail.com>
2025-04-15 21:08:44 +0200

    اللّٰه، سورِيا و بشار

--- Programowanie/Matura2022CzerwiecConsoleApplication/Matura2022CzerwiecConsoleApplication.cpp
+++ Programowanie/Matura2022CzerwiecConsoleApplication/Matura2022CzerwiecConsoleApplication.cpp
@@ -2,123 +2,75 @@
 #include <fstream>
 #include <vector>
 
-// Zadanie 4.1
-void zadanie4_1() {
+int getReversed(int number) {
+    int reversed = 0;
+    while (number > 0) {
+        int digit = number % 10;
+        reversed = reversed * 10 + digit;
+        number /= 10;
+    }
+    return reversed;
+}
+
+bool isPrime(int n) {
+    if (n < 2) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+
+int main() {
     std::ifstream file("liczby.txt");
     std::vector<int> numbers;
     int number;
-
-    std::cout << "Zadanie 4.1\n";
-    while (file >> number) {
-        int reversed = 0;
-        int temp = number;
-        while (temp > 0) {
-            int digit = temp % 10;
-            reversed = reversed * 10 + digit;
-            temp /= 10;
-        }
-        if (reversed % 17 == 0) {
-            std::cout << reversed << "\n";
-        }
-    }
-    file.close();
-}
-
-// Zadanie 4.2
-void zadanie4_2() {
-    std::ifstream file("liczby.txt");
-    std::vector<int> numbers;
-    int number;
-
-    int maxDifference = 0;
-    int numberWithMaxDifference = 0;
-    std::cout << "Zadanie 4.2\n";
-
-    while (file >> number) {
-        int reversed = 0;
-        int temp = number;
-        while (temp > 0) {
-            int digit = temp % 10;
-            reversed = reversed * 10 + digit;
-            temp /= 10;
-        }
-        int difference = number - reversed;
-        if (difference < 0) {
-            difference = -difference;
-        }
-        if (difference > maxDifference) {
-            maxDifference = difference;
-            numberWithMaxDifference = number;
-        }
-    }
-    std::cout << numberWithMaxDifference << " " << maxDifference << "\n";
-    file.close();
-}
-
-// zadanie 4.3
-void zadanie4_3() {
-    std::ifstream file("liczby.txt");
-    int number;
-
-    std::cout << "Zadanie 4.3\n";
-
-    while (file >> number) {
-        bool isPrime = true;
-        if (number < 2) {
-            isPrime = false;
-        }
-        else {
-            for (int i = 2; i * i <= number; i++) {
-                if (number % i == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-        }
-
-        if (!isPrime) {
-            continue;
-        }
-
-        int reversed = 0;
-        int temp = number;
-        while (temp > 0) {
-            int digit = temp % 10;
-            reversed = reversed * 10 + digit;
-            temp /= 10;
-        }
-        bool isReversedPrime = true;
-        if (reversed < 2) {
-            isReversedPrime = false;
-        }
-        else {
-            for (int i = 2; i * i <= reversed; i++) {
-                if (reversed % i == 0) {
-                    isReversedPrime = false;
-                    break;
-                }
-            }
-        }
-        if (isReversedPrime) {
-            std::cout << number << "\n";
-        }
-    }
-    file.close();
-}
-
-// Zadanie 4.4
-void zadanie4_4() {
-    std::ifstream file("liczby.txt");
-    std::vector<int> numbers;
-    int number;
-
     while (file >> number) {
         numbers.push_back(number);
     }
     file.close();
 
-    std::cout << "Zadanie 4.4\n";
+    // Zadanie 4.1
+    std::cout << "Zadanie 4.1\n";
+    for (int i = 0; i < numbers.size(); i++) {
+        int reversed = getReversed(numbers[i]);
+        if (reversed % 17 == 0) {
+            std::cout << reversed << "\n";
+        }
+    }
 
+    // Zadanie 4.2
+    std::cout << "Zadanie 4.2\n";
+    int maxDifference = 0;
+    int numberWithMaxDifference = -1;
+    for (int i = 0; i < numbers.size(); i++) {
+        int reversed = getReversed(numbers[i]);
+        int difference = numbers[i] - reversed;
+        if (difference < 0) {
+            difference = -difference;
+        }
+        if (difference > maxDifference) {
+            maxDifference = difference;
+            numberWithMaxDifference = numbers[i];
+        }
+    }
+    if (numberWithMaxDifference != -1) {
+        std::cout << numberWithMaxDifference << " " << maxDifference << "\n";
+    }
+
+    // Zadanie 4.3
+    std::cout << "Zadanie 4.3\n";
+    for (int i = 0; i < numbers.size(); i++) {
+        if (isPrime(numbers[i])) {
+            int reversed = getReversed(numbers[i]);
+            if (isPrime(reversed)) {
+                std::cout << numbers[i] << "\n";
+            }
+        }
+    }
+
+    // Zadanie 4.4
+    std::cout << "Zadanie 4.4\n";
     std::vector<int> uniqueNumbers;
     std::vector<int> counts;
 
@@ -150,12 +102,6 @@ void zadanie4_4() {
         }
     }
     std::cout << uniqueCount << " " << countTwo << " " << countThree << "\n";
-}
 
-int main() {
-    zadanie4_1();
-    zadanie4_2();
-    zadanie4_3();
-    zadanie4_4();
     return 0;
 }
