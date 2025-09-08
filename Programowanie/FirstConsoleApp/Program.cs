ab3715479c9c7d319a5ed1226362fe6ab98b2930
Sala213 <Sala213@ezse.pl>
2025-09-08 13:18:50 +0200

    wololo

---
+++ Programowanie/FirstConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Hello, World!\n");
+Console.WriteLine("Hello, World!");
+
+string name = "Jan";
+string surname = "Kowalski";
+Console.WriteLine("Wassap" + " " + name + " " + surname + " " + "skibidi");
