eba327ba3044493c0db05c641607fe8f94eedec4
Sala213 <Sala213@ezse.pl>
2025-09-10 15:53:46 +0200

    sraken pierdaken

--- Programowanie/FirstConsoleApp/Program.cs
+++ Programowanie/FirstConsoleApp/Program.cs
@@ -5,3 +5,31 @@ Console.WriteLine("Hello, World!");
 string name = "Jan";
 string surname = "Kowalski";
 Console.WriteLine("Wassap" + " " + name + " " + surname + " " + "skibidi");
+Console.WriteLine("Witaj {0} {1} tutaj!!!!", name, surname);
+Console.WriteLine($"Witaj {name} {surname} tutaj!!!!");
+
+Console.WriteLine("Prędkość to km\\h");
+Console.WriteLine(@"Prędkość to km\h");
+
+int firstNumber = 5;
+int secondNumber = firstNumber;
+secondNumber++;
+Console.WriteLine($"Pierwsza liczba to {firstNumber}");
+Console.WriteLine($"Druga liczba to {secondNumber}");
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+//przekazywanie przez wartość
+
+void ParametrTest_v1(int p)
+{
+    Console.WriteLine($"Parametr w parametrTest_v1 {p}");
+    p++;
+    Console.WriteLine($"Parametr w parametrTest_v1 {p}");
+}
+
+firstNumber = 15;
+Console.WriteLine($"firstNumber przed {firstNumber}");
+ParametrTest_v1(firstNumber);
+Console.WriteLine($"firstNumber po {firstNumber}");
+
