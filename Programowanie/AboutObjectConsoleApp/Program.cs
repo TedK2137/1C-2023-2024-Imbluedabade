7626752bee800c2faed42f1bea903602a318ff74
Sala213 <Sala213@ezse.pl>
2025-09-22 13:13:51 +0200

    skibidi

---
+++ Programowanie/AboutObjectConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+
+namespace AboutObjectConsoleApp;
+
+internal class Program
+{
+    private int x;
+    private int y;
+
+    public Point()
+    {
+        x = 5;
+        y = 8;
+    }
+
+    public Point(int a, int b)
+    {
+        x = a;
+        y = b;
+    }
+
+    public void Show()
+    {
+               Console.WriteLine($"x = {x}, y = {y}");
+    }
+}
