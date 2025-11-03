f7ca973ccc9987273035d2d10e0a41a3b6356ae1
TedK2137 <stachurakuba0@gmail.com>
2025-11-03 08:13:49 +0100

    wololo

--- Programowanie/PracticalTasksConsoleApp/Styczen2023_1/Task1.cs
+++ Programowanie/PracticalTasksConsoleApp/Styczen2023_1/Task1.cs
@@ -11,9 +11,36 @@ namespace PracticalTasksConsoleApp.Styczen2023_1
         public void Task()
         {
             Console.WriteLine("Wprowadź liczbę a:");
-            int a = 6;
-            Console.WriteLine("Wprowadź liczbę a:");
-
+            int a = int.Parse(Console.ReadLine());
+            if (a < 0)
+                Console.WriteLine("Podaj dodatnie liczby");
+            Console.WriteLine("Wprowadź liczbę b:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj dodatnie liczby");
+            Console.WriteLine($"NWD liczb wynosi: {NWD(a, b)}");
         }
+
+        public static int NWD(int a, int b)
+        {
+            while (a != b)
+            {
+                if (a > b)
+                {
+                    a -= b;
+                }
+                else
+                    b -= a;
+            }
+            return a;
+        }
+
+        /************************************************************************
+        Nazwa funkcji: NWD
+        Opis funkcji: Szuka największego wspólnego dzielnika podanych liczb
+        Parametry: a - pierwsza liczba, b - druga liczba
+        Zwracany typ i opis: zwraca zmienną typu int, liczbę będącą największym wspólnym dzielnikiem
+        Autor: 1234567890
+        ************************************************************************/
     }
 }
+
