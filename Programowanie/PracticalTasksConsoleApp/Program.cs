3730a6fe4271862846931c714581b48fabf18755
TedK2137 <stachurakuba0@gmail.com>
2025-11-17 02:48:29 +0100

    wololo2

--- Programowanie/PracticalTasksConsoleApp/Program.cs
+++ Programowanie/PracticalTasksConsoleApp/Program.cs
@@ -5,6 +5,7 @@ using PracticalTasksConsoleApp.Czerwiec2022_06;
 using PracticalTasksConsoleApp.Czerwiec2023_06;
 using PracticalTasksConsoleApp.Styczen2023_1;
 using PracticalTasksConsoleApp.Styczeń2023_1Mobilna;
+using PracticalTasksConsoleApp.Styczen2025_1;
 
 //Task1 task1 = new Task1();
 //task1.Task();
@@ -15,5 +16,8 @@ using PracticalTasksConsoleApp.Styczeń2023_1Mobilna;
 //Task3 task3 = new Task3();
 //task3.Task();
 
-Task4 task4 = new Task4();
-task4.Task();
+//Task4 task4 = new Task4();
+//task4.Task();
+
+Task5 task5 = new Task5();
+task5.Task();
---
+++ Programowanie/PracticalTasksConsoleApp/Styczen2025_1/Task5.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalTasksConsoleApp.Styczen2025_1
+{
+    class Device
+    {
+        /******************************************************************** 
+         nazwa:                 <displayMessage>
+         opis:                  <Metoda wyświetlająca komunikat na konsoli>
+         parametry:             <string message - komunikat do wyświetlenia>
+         zwracany typ i opis:   <brak>
+         autor:                 <101110>
+        ********************************************************************/
+        public void displayMessage(string message)
+        {
+            Console.WriteLine(""+message);
+        }
+    }
+    class WashingMashine : Device
+    {
+        private int washingProgram = 0;
+
+        public int setWashingProgram(int washingProgram)
+        {
+            if (washingProgram < 1 || washingProgram > 12)
+            {
+                displayMessage("Podano niepoprawny numer programu");
+                this.washingProgram = 0;
+            }
+            else
+            {
+                this.washingProgram = washingProgram;
+                displayMessage("Program został ustawiony");
+            }
+            return this.washingProgram;
+        }
+    }
+    class Vacuum : Device
+    {
+        private bool vacuumStatus = false;
+
+        public void On()
+        {
+            if (vacuumStatus == false)
+            {
+                vacuumStatus = true;
+                displayMessage("Odkurzacz włączono");
+            }
+        }
+
+        public void Off()
+        {
+            if (vacuumStatus == true)
+            {
+                vacuumStatus = false;
+                displayMessage("Odkurzacz wyłączono");
+            }
+            else
+            {
+                displayMessage("Odkurzacz jest już wyłączony");
+            }
+        }
+    }
+    class Task5
+    {
+        public void Task()
+        {
+            WashingMashine washingMashine = new WashingMashine();
+            Vacuum vacuum = new Vacuum();
+
+            Console.WriteLine("Podaj numer programu prania (1-12): ");
+            washingMashine.setWashingProgram(int.Parse(Console.ReadLine()));
+
+            vacuum.On();
+            vacuum.On();
+            vacuum.On();
+
+            vacuum.displayMessage("Odkurzacz wyładował się");
+
+            vacuum.Off();
+        }
+    }
+}
