2c37a59c6d546431aa4304cc22843f988d4d9502
4C <Uczen@LAPTOP-26CG11IL>
2026-09-17 12:59:48 +0200

    ae

---
+++ Programowanie/SimpleBindingCalculatorMauiApp/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿namespace SimpleBindingCalculatorMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public string StrFirstNumber { get; set; }
+        public string StrSecondNumber { get; set; }
+
+        private string resultMessage;
+
+        public string ResultMessage
+        {
+            get { return resultMessage; }
+            set
+            {
+                resultMessage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Color returnColor;
+
+        public Color ReturnColor
+        {
+            get { return returnColor; }
+            set { returnColor = value; OnPropertyChanged(); }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //pobrać dane wejściowe
+            //var firstNumber = int.Parse(firstNumberEntry.Text);
+            //int firstNumber;
+            if (int.TryParse(StrFirstNumber, out int firstNumber)
+                && int.TryParse(StrSecondNumber, out int secondNumber))
+            {
+                //wykonać operację dodawania
+                //wyświetlić wynik
+                ResultMessage = $"Wynik dodawania to {firstNumber + secondNumber}";
+                ReturnColor = Colors.Green;
+            }
+            else
+            {
+                ResultMessage = $"Podałeś nieprawidłowe dane.";
+                ReturnColor = Colors.Red;
+            }
+
+        }
+    }
+}
